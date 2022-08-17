81a1c35b734b67bd28442403102bc13c81561843
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-17 15:36:42 +0530

    PBA-7:Create View Files with presentation logics

--- src/main/java/com/PallaviAnde/Controller/ContactController.java
+++ src/main/java/com/PallaviAnde/Controller/ContactController.java
@@ -1,13 +1,15 @@
 package com.PallaviAnde.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.PallaviAnde.Service.ContactService;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/api/phoneBook")
 public class ContactController {
 
