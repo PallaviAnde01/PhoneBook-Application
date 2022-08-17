6a11071aceb0536b783595bc0f6db70cd9c2cc31
PallaviAnde01 <pallaviande12393@gmail.com>
2022-08-17 14:04:25 +0530

    PBA-2:Create project and required package structure

---
+++ src/test/java/com/PallaviAnde/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PallaviAnde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
